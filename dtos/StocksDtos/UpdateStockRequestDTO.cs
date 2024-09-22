80a00e4bb83ee184e94b98585fface5313847dca
M7MDNoureddine <mohamadnoureddine@graduate.utm.my>
2024-09-22 12:26:38 +0300

    Add all CRUD operations.

---
+++ dtos/StocksDtos/UpdateStockRequestDTO.cs
@@ -0,0 +1,18 @@
+namespace firstapi.dtos.StocksDtos
+{
+    public class UpdateStockRequestDTO
+    {
+
+        public string Symbol { get; set; } = string.Empty;
+
+        public string CompanyName { get; set; } = string.Empty;
+
+        public decimal Purchase { get; set; }
+
+        public decimal LastDiv { get; set; }
+
+        public string Industry { get; set; } = string.Empty;
+
+        public long MarketCap { get; set; }
+    }
+} 
